adaacebb0d98e2b7f3cb64276c475bf28c75cb8f
changfeng <louchangfeng@outlook.com>
2022-04-18 20:47:32 +0800

    feature(Vector): add constructor, destructor and copy constructor for Vector
    extract the method `copyFrom()` that is responsible for the copy constructor of class

--- src/DataStructure/Vector.hpp
+++ src/DataStructure/Vector.hpp
@@ -7,13 +7,57 @@ typedef int Rank;
 namespace dsa {
     template<typename T>
     class Vector {
-    private:
+    protected:
         Rank m_size;
-        int  capacity;
+        Rank m_capacity;
         T   *m_elem;
 
+        void copyFrom(T const *A, Rank lo, Rank hi);
+
     public:
-    public:
-        Vector(){};
+        Vector(Rank c = DEFAULT_CAPACITY, Rank s = 0, T v = 0) {
+            m_size = 0;
+            m_capacity = c;
+            m_elem = new T[m_capacity];
+
+            while (m_size < s) {
+                m_elem[m_size] = v;
+                m_size++;
+            }
+        }
+
+        Vector(T const *A, Rank n) {
+            copyFrom(A, 0, n);
+        }
+
+        Vector(T const *A, Rank lo, Rank hi) {
+            copyFrom(A, lo, hi);
+        }
+
+        Vector(const Vector<T> &V) {
+            copyFrom(V.m_elem, 0, V.m_size);
+        }
+
+        Vector(const Vector<T> &V, Rank lo, Rank hi) {
+            copyFrom(V.m_elem, lo, hi);
+        }
+
+        ~Vector() {
+            delete[] m_elem;
+        }
     };
+
+    template<typename T>
+    void Vector<T>::copyFrom(const T *A, Rank lo, Rank hi) {
+        m_size = 0;
+        m_capacity = (hi - lo) << 1;
+        m_elem = new T[m_capacity];
+
+        while (lo < hi) {
+            m_elem[m_size] = A[lo];
+
+            m_size++;
+            lo++;
+        }
+    }
 }  // namespace dsa
