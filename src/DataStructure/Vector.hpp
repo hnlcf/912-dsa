85907a5704d7512000efdeb2e0e0540ee2f271ec
changfeng <louchangfeng@outlook.com>
2022-04-18 20:14:38 +0800

    git init repo

---
+++ src/DataStructure/Vector.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef int Rank;
+
+#define DEFAULT_CAPACITY 3
+
+namespace dsa {
+    template<typename T>
+    class Vector {
+    private:
+        Rank m_size;
+        int  capacity;
+        T   *m_elem;
+
+    public:
+    public:
+        Vector(){};
+    };
+}  // namespace dsa
---
+++ src/dsa.cpp
@@ -0,0 +1,11 @@
+//
+// Created by changfeng on 2022/4/18.
+//
+
+#include <iostream>
+
+int
+main(int argc, char **argv) {
+    std::cout << "Hello world\n";
+    return 0;
+}
