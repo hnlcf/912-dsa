333ba3e16c5ea1524484eaa32d8bbf986cf77342
changfeng <louchangfeng@outlook.com>
2022-04-30 21:57:13 +0800

    feature(Stack): implement a Stack using inheritance based on Vector (#13)

--- src/DataStructure/Stack.hpp
+++ src/DataStructure/Stack.hpp
@@ -1,8 +1,29 @@
 #pragma once
 
+#include "Vector.hpp"
+
 namespace dsa {
     template<typename T>
-    class Stack {
+    class Stack : public Vector<T> {
 
+    public:
+        void push(T const &e);
+        T    pop();
+        T   &top();
     };
+
+    template<typename T>
+    void Stack<T>::push(const T &e) {
+        this->insert(this->size(), e);
+    }
+
+    template<typename T>
+    T Stack<T>::pop() {
+        return this->remove(this->size() - 1);
+    }
+
+    template<typename T>
+    T &Stack<T>::top() {
+        return (*this)[this->size() - 1];
+    }
 }
