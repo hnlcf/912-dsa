6c8899463fd4fcf0efedd0a4a1992469c2b10457
Changfeng <louchangfeng@outlook.com>
2022-05-26 13:05:55 +0800

    CI: complete workflow
    1. update catch2 test command
    2. modify test for CI to report error

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -4,7 +4,7 @@
 TEST_CASE("List can be initialized", "[List]") {
     SECTION("empty list") {
         dsa::List<int> l;
-        REQUIRE(l.size() == 0);
+        REQUIRE(l.size() == 1);
         REQUIRE(l.first() == l.last()->m_succ);
         REQUIRE(l.last() == l.first()->m_pred);
     }
