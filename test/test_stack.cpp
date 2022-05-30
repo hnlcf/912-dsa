417a243515499b256cda7757f80425e29f9aecec
Changfeng <louchangfeng@outlook.com>
2022-05-30 22:07:59 +0800

    test(StackExample): add test case for `evaluate` function (#24)

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -52,3 +52,32 @@ TEST_CASE("Match brackets in string", "[stack][example]") {
         REQUIRE(dsa::matchBrackets(s4) == false);
     }
 }
+
+TEST_CASE("Evaluate a math expression", "[stack][example]") {
+    SECTION("All operators") {
+        std::string str = "(1+2^3!-4)*(5!-(6-(7-(89-0!))))";
+        REQUIRE(dsa::evaluate(str) == 2013.0);
+    }
+    SECTION("+ - operators") {
+        std::string str = "1+2-6";
+        REQUIRE(dsa::evaluate(str) == -3.0);
+    }
+    SECTION("* / operators") {
+        std::string str = "2*6/3";
+        REQUIRE(dsa::evaluate(str) == 4.0);
+    }
+    SECTION("^ operators") {
+        std::string str = "2^3";
+        REQUIRE(dsa::evaluate(str)  == 8.0);
+    }
+
+    SECTION("! operators") {
+        std::string str = "3!-0!";
+        REQUIRE(dsa::evaluate(str)  == 5.0);
+    }
+
+    SECTION("() operators") {
+        std::string str = "4*(3-5)";
+        REQUIRE(dsa::evaluate(str)  == -8.0);
+    }
+}
