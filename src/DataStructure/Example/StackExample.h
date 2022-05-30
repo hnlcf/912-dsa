a5b58320ba558ce94f9236a172494650d8494f5b
Changfeng <louchangfeng@outlook.com>
2022-05-30 22:07:19 +0800

    feature(StackExample): add `evaluate`, a stack application (#24)
    Main function:
    
    - `evaluate`: calculate the math expression
    
    Assist function:
    
    - `isDigit`: Determine if the given character is a number
    - `readNumber`: Read a continuous number from a specific position in string, and store with stack
    - `orderBetween`: Compare the calculate priorities of TWO operators
    - `calculate`: (two overloads)
      - Calculate a math expression with ONE operator(factorial) and ONE operand.
      - Calculate a math expression with ONE operator and TWO operands.
    
    Assist struct:
    
    - `MathOperator`: include basic math operators and comparison operations

--- src/DataStructure/Example/StackExample.h
+++ src/DataStructure/Example/StackExample.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Stack.hpp"
+#include <cmath>
 #include <cstdint>
 #include <string>
 
@@ -18,4 +19,213 @@ namespace dsa {
     /// @param expr The string expression to be matched
     /// @return If all pairs matched return ture, otherwise false.
     bool matchBrackets(const std::string &expr);
+
+    /// @brief Compute a math expression
+    /// @param exp The math expression given
+    /// @return Result of math expression
+    double evaluate(const std::string &exp);
+
+    /// @brief Define the math operators with corresponding orders of priority.
+    struct MathOperator {
+        enum class Opr : uint32_t {
+            None = 0xffff,
+            Add = 0x0001,           /// `+`
+            Sub = 0x0002,           /// `-`
+            Mul = 0x0011,           /// `*`
+            Div = 0x0012,           /// `/`
+            Exp = 0x0101,           /// `^`
+            Fac = 0x0102,           /// `!`
+            LeftBracket = 0x1001,   /// `(`
+            RightBracket = 0x1002,  /// `)`
+            Eof = 0x00,             /// `\0`
+        };
+
+        enum class Order : uint8_t {
+            None = 0x00,   /// ` `, the blank
+            Less = 0x01,   /// `<`
+            More = 0x02,   /// `>`
+            Equal = 0x04,  /// `=`
+        };
+
+        /// @brief Convert a character to corresponding math operator.
+        static Opr parseOperator(const char ch) {
+            Opr opr = Opr::None;
+            switch (ch) {
+                case '+':
+                    opr = Opr::Add;
+                    break;
+                case '-':
+                    opr = Opr::Sub;
+                    break;
+                case '*':
+                    opr = Opr::Mul;
+                    break;
+                case '/':
+                    opr = Opr::Div;
+                    break;
+                case '^':
+                    opr = Opr::Exp;
+                    break;
+                case '!':
+                    opr = Opr::Fac;
+                    break;
+                case '(':
+                    opr = Opr::LeftBracket;
+                    break;
+                case ')':
+                    opr = Opr::RightBracket;
+                    break;
+                case '\0':
+                    opr = Opr::Eof;
+                    break;
+                default:
+                    break;
+            }
+            return opr;
+        }
+
+        /// @brief Compare the order of two math operators according to priorities.
+        /// @param lhs Left operator
+        /// @param rhs Right operator
+        /// @return The enumeration member corresponding to order.
+        static Order orderBetweenOperator(const MathOperator::Opr &lhs,
+                                          const MathOperator::Opr &rhs) {
+            Order ans = Order::None;
+            switch (lhs) {
+                case Opr::Add:
+                case Opr::Sub: {
+                    switch (rhs) {
+                        case Opr::Add:
+                        case Opr::Sub:
+                        case Opr::RightBracket:
+                        case Opr::Eof:
+                            ans = Order::More;
+                            break;
+                        case Opr::Mul:
+                        case Opr::Div:
+                        case Opr::Exp:
+                        case Opr::Fac:
+                        case Opr::LeftBracket:
+                            ans = Order::Less;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                case Opr::Mul:
+                case Opr::Div: {
+                    switch (rhs) {
+                        case Opr::Add:
+                        case Opr::Sub:
+                        case Opr::Mul:
+                        case Opr::Div:
+                        case Opr::RightBracket:
+                        case Opr::Eof:
+                            ans = Order::More;
+                            break;
+                        case Opr::Exp:
+                        case Opr::Fac:
+                        case Opr::LeftBracket:
+                            ans = Order::Less;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                case Opr::Exp: {
+                    switch (rhs) {
+                        case Opr::Add:
+                        case Opr::Sub:
+                        case Opr::Mul:
+                        case Opr::Div:
+                        case Opr::Exp:
+                        case Opr::RightBracket:
+                        case Opr::Eof:
+                            ans = Order::More;
+                            break;
+                        case Opr::Fac:
+                        case Opr::LeftBracket:
+                            ans = Order::Less;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                case Opr::Fac: {
+                    switch (rhs) {
+                        case Opr::Add:
+                        case Opr::Sub:
+                        case Opr::Mul:
+                        case Opr::Div:
+                        case Opr::Exp:
+                        case Opr::Fac:
+                        case Opr::RightBracket:
+                        case Opr::Eof:
+                            ans = Order::More;
+                            break;
+                        case Opr::LeftBracket:
+                            ans = Order::None;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                case Opr::LeftBracket: {
+                    switch (rhs) {
+                        case Opr::Add:
+                        case Opr::Sub:
+                        case Opr::Mul:
+                        case Opr::Div:
+                        case Opr::Exp:
+                        case Opr::Fac:
+                        case Opr::LeftBracket:
+                            ans = Order::Less;
+                            break;
+                        case Opr::RightBracket:
+                            ans = Order::Equal;
+                            break;
+                        case Opr::Eof:
+                            ans = Order::More;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                case Opr::RightBracket: {
+                    ans = Order::None;
+                    break;
+                }
+                case Opr::Eof: {
+                    switch (rhs) {
+                        case Opr::Add:
+                        case Opr::Sub:
+                        case Opr::Mul:
+                        case Opr::Div:
+                        case Opr::Exp:
+                        case Opr::Fac:
+                        case Opr::LeftBracket:
+                            ans = Order::Less;
+                            break;
+                        case Opr::RightBracket:
+                            ans = Order::None;
+                            break;
+                        case Opr::Eof:
+                            ans = Order::Equal;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                case Opr::None:
+                default:
+                    break;
+            }
+            return ans;
+        }
+    };
 }
