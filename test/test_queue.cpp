0c71d46b8d234e5337ddcda568ceed43dfed79fb
Changfeng <louchangfeng@outlook.com>
2022-06-01 20:21:05 +0800

    feature(queue): create `Queue` template class (#26)
    Add:
    - Implementation of queue
    - Test case of queue
    
    Modify:
    - CMake config of test, add `test_queue.cpp` to source file list

---
+++ test/test_queue.cpp
@@ -0,0 +1,21 @@
+#include "Queue.hpp"
+#include "catch2/catch.hpp"
+
+TEST_CASE("Queue: basic methods", "[queue]") {
+    dsa::Queue<int> q;
+    q.enqueue(1);
+
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.isEmpty() == false);
+    REQUIRE(q.first() == 1);
+    REQUIRE(q.last() == 1);
+
+    auto tmp = q.dequeue();
+
+    REQUIRE(tmp == 1);
+    REQUIRE(q.size() == 0);
+    REQUIRE(q.isEmpty() == true);
+
+    auto p = q.first();
+    REQUIRE(p == -1);
+}
