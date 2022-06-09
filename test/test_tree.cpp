4e225e58899d9793f7d26128edcf75e3fb8fe3c9
Changfeng <louchangfeng@outlook.com>
2022-06-09 23:28:06 +0800

    test(tree): add test case for construct, insert of `BinTreeNode`
    Modify the explanations in test cases of traverse

--- test/test_tree.cpp
+++ test/test_tree.cpp
@@ -4,17 +4,91 @@
 using dsa::BinTree;
 using dsa::BinTreeNode;
 
-TEST_CASE("BinTreeNode: Initialize", "[bin-tree]") {
-    dsa::BinTree<int> p;
+TEST_CASE("BinTreeNode: Constructor", "[bin-tree]") {
+    SECTION("BinTreeNode: Empty Constructor") {
+        BinTreeNode<int> p;
 
-    REQUIRE(p.root() != nullptr);
-    REQUIRE(p.size() == 1);
-    REQUIRE(!p.isEmpty());
+        REQUIRE(p.m_data == 0);
+        REQUIRE(p.m_height == 0);
+        REQUIRE(p.m_parent == nullptr);
+        REQUIRE(p.m_left == nullptr);
+        REQUIRE(p.m_right == nullptr);
+        REQUIRE(p.size() == 1);
+    }
 
-    p.insertAsRightNode(p.root(), 3);
+    SECTION("BinTreeNode: Construct with data and parent") {
+        BinTreeNode<int> p(-1, nullptr);
 
-    REQUIRE(p.size() == 2);
-    REQUIRE(p.root()->m_height == 1);
+        REQUIRE(p.m_data == -1);
+        REQUIRE(p.m_height == 0);
+        REQUIRE(p.m_parent == nullptr);
+        REQUIRE(p.m_left == nullptr);
+        REQUIRE(p.m_right == nullptr);
+    }
+
+    SECTION("BinTreeNode: Construct with all fields") {
+        BinTreeNode<int> p(99, 0, nullptr, nullptr, nullptr);
+
+        REQUIRE(p.m_data == 99);
+        REQUIRE(p.m_height == 0);
+        REQUIRE(p.m_parent == nullptr);
+        REQUIRE(p.m_left == nullptr);
+        REQUIRE(p.m_right == nullptr);
+    }
+}
+
+TEST_CASE("BinTreeNode: Insert Node", "[bin-tree]") {
+    SECTION("BinTreeNode: Insert left") {
+        BinTreeNode<int> p;
+
+        REQUIRE(p.m_left == nullptr);
+
+        auto *tmp = p.insertAsLeftChild(99);
+        REQUIRE(p.m_left->m_data == 99);
+        REQUIRE(tmp == p.m_left);
+        REQUIRE(p.size() == 2);
+
+        p.insertAsLeftChild(-99);
+        REQUIRE(p.m_left->m_data == -99);
+        REQUIRE(tmp == p.m_left);
+        REQUIRE(p.size() == 2);
+    }
+
+    SECTION("BinTreeNode: Insert right") {
+        BinTreeNode<int> p;
+
+        REQUIRE(p.m_right == nullptr);
+
+        auto *tmp = p.insertAsRightChild(99);
+        REQUIRE(p.m_right->m_data == 99);
+        REQUIRE(tmp == p.m_right);
+        REQUIRE(p.size() == 2);
+
+        p.insertAsRightChild(-99);
+        REQUIRE(p.m_right->m_data == -99);
+        REQUIRE(tmp == p.m_right);
+        REQUIRE(p.size() == 2);
+    }
+
+    SECTION("BinTreeNode: Insert both left and right") {
+        BinTreeNode<int> p;
+
+        auto *tmp1 = p.insertAsLeftChild(1);
+        auto *tmp2 = p.insertAsRightChild(-1);
+
+        REQUIRE(p.m_left->m_data == 1);
+        REQUIRE(p.m_right->m_data == -1);
+        REQUIRE(tmp1 == p.m_left);
+        REQUIRE(tmp2 == p.m_right);
+        REQUIRE(p.size() == 3);
+
+        p.insertAsLeftChild(-99);
+        p.insertAsRightChild(99);
+        REQUIRE(p.m_left->m_data == -99);
+        REQUIRE(p.m_right->m_data == 99);
+        REQUIRE(tmp1 == p.m_left);
+        REQUIRE(tmp2 == p.m_right);
+    }
 }
 
 template<class T>
@@ -46,7 +120,7 @@ TEST_CASE("BinTreeNode: Four Traverse", "[bin-tree]") {
 
     GetTraverse<char> visit;
 
-    SECTION("BinTreeNode: preorder traverse") {
+    SECTION("BinTreeNode: Preorder traverse") {
         visit.clear();
         root.traversePreorderRecur(&root, visit);
         REQUIRE(visit.m_data == "125784369");
@@ -60,7 +134,7 @@ TEST_CASE("BinTreeNode: Four Traverse", "[bin-tree]") {
         REQUIRE(visit.m_data == "125784369");
     }
 
-    SECTION("BinTreeNode: inorder traverse") {
+    SECTION("BinTreeNode: Inorder traverse") {
         visit.clear();
         root.traverseInorderRecur(&root, visit);
         REQUIRE(visit.m_data == "758241396");
@@ -70,7 +144,7 @@ TEST_CASE("BinTreeNode: Four Traverse", "[bin-tree]") {
         REQUIRE(visit.m_data == "758241396");
     }
 
-    SECTION("BinTreeNode: postorder traverse") {
+    SECTION("BinTreeNode: Postorder traverse") {
         visit.clear();
         root.traversePostorderRecur(&root, visit);
         REQUIRE(visit.m_data == "785429631");
@@ -80,7 +154,7 @@ TEST_CASE("BinTreeNode: Four Traverse", "[bin-tree]") {
         REQUIRE(visit.m_data == "785429631");
     }
 
-    SECTION("BinTreeNode: level traverse") {
+    SECTION("BinTreeNode: Level traverse") {
         visit.clear();
         root.traverseLevel(&root, visit);
         REQUIRE(visit.m_data == "123546789");
