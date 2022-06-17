e097fc9df84a4722e495cff6bb746ee4a5900dba
Changfeng <louchangfeng@outlook.com>
2022-06-17 21:10:34 +0800

    refactor(test): classify test files, create main test file

--- test/basic/test_list.cpp
+++ test/basic/test_list.cpp
@@ -1,5 +1,5 @@
-#include "List.hpp"
-#include "catch2/catch_test_macros.hpp"
+#include <List.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("List initialize", "[list]") {
     SECTION("empty list") {
---
+++ test/graph/test_graph_martix.cpp
@@ -0,0 +1,5 @@
+#include <GraphMatrix.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("GraphMatrix: Initialize", "[graph]") {
+}
