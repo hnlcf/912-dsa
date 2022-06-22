ff40fa7740f771f8e83aa6f2eb71ba6a5b1d391b
Changfeng <louchangfeng@outlook.com>
2022-06-22 20:12:05 +0800

    feat(avl): implement the `insert` method of avl

--- src/DataStructure/AVL.hpp
+++ src/DataStructure/AVL.hpp
@@ -8,9 +8,34 @@ namespace dsa {
     using Node = BinTreeNode<T> *;
 
 public:
-
     Node insert(T const &e) override {
-      return nullptr;
+      auto &target = this->search(e);
+
+      // If already exists, directly return.
+      if (target != nullptr) {
+        return target;
+      }
+
+      // Otherwise create a new node and update size.
+      target = new BinTreeNode<T>(e, this->m_hot);
+      this->m_size++;
+
+      // Create a copy of new node for return in the end.
+      Node copy = target;
+
+      // Determine whether AVL tree is balanced or not. If not, rotate to rebalance.
+      // Otherwise, just update tree height.
+      for (auto par = target->m_parent; par != nullptr; par = par->m_parent) {
+        if (!isAVLBalance(par)) {
+          // TODO: rotate
+
+          break;
+        } else {
+          this->updateHeight(par);
+        }
+      }
+
+      return copy;
     }
 
     bool remove(T const &e) override {
