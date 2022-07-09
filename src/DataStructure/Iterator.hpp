ff07a279fd8b259a01b637c1129e2248a9b103b7
ravenxrz <zhang.xingrui@foxmail.com>
2022-07-09 23:54:50 +0800

    fix(iterator): remove the inheritance of `std::iterator`
    C++17 deprecate the usage of `std::iterator` in custom iterator, we can manually declare necessary type aliases to implement it.

--- src/DataStructure/Iterator.hpp
+++ src/DataStructure/Iterator.hpp
@@ -4,14 +4,14 @@
 
 namespace dsa {
   template<class T>
-  class Iterator : public std::iterator<std::input_iterator_tag, T> {
+  class Iterator {
 public:
     /* An iterator class must declare(public) the following types */
     using difference_type = std::ptrdiff_t;
     using value_type = T;
     using pointer = T *;
     using reference = T &;
-    using iterator_category = std::output_iterator_tag;
+    using iterator_category = std::forward_iterator_tag;
 
 public:
     /* An iterator class must implement(public) the following operator overloads */
