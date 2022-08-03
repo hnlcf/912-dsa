6a5c0d9bfde301c561cad0c24319dd946f76e0d7
Changfeng <louchangfeng@outlook.com>
2022-08-03 11:49:33 +0800

    Add test case for splay tree

---
+++ test/tree/test_splaytree.cc
@@ -0,0 +1,27 @@
+#include <test_header.h>
+
+using dsa::SplayTree;
+using dsa::Vector;
+
+TEST_CASE("Splay: demo", "[bst][Splay]") {
+  SplayTree<int> s;
+
+  s.Insert(9999);
+  s.Insert(99);
+  s.Insert(27);
+  s.Insert(1);
+  s.Insert(0);
+  s.Insert(-3);
+  s.Insert(-54);
+
+  GetTraverse<int> visit;
+
+  visit.Clear();
+  s.TraverseInorder(visit);
+
+  s.Search(1);
+  Vector<int> actual = visit.m_data;
+  Vector<int> expect = {-54, -3, 0, 1, 27, 99, 9999};
+
+  REQUIRE(actual == expect);
+}
